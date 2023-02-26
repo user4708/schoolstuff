278e41c6d2a6662c9f66d5a1568268bbac68a31d
Riley <92556890+user4708@users.noreply.github.com>
2023-02-26 17:20:06 -0600

    Create CalcTest.java

---
+++ CalcTest.java
@@ -0,0 +1,41 @@
+// Riley Olson
+// SE470
+// JUnit for Calc.java
+
+package junitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+import org.junit.jupiter.api.Test;
+
+class CalcTest {
+
+	   @Test public void testAdd()
+	   {
+	      assertTrue ("Calc sum incorrect", 4 == Calc.add (2, 2));
+	   }
+	   
+	   @Test public void testSubstract()
+	   {
+	      assertTrue ("Calc substraction incorrect", 0 == Calc.substract (2, 2));
+	   }
+	   
+	   @Test public void testMultiply()
+	   {
+	      assertTrue ("Calc multiplication incorrect", 12 == Calc.multiply (2, 6));
+	   }
+	   
+	   @Test public void testDivide()
+	   {
+		  try {
+			  assertTrue ("Calc division incorrect", 10 == Calc.divide (100, 10));  //Causes underflow
+		  }
+		  catch(ArithmeticException e) {
+			  return;
+		  }
+		  fail("ArithmeticException expected");
+	   }
+
+}
