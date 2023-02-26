0ad575a875b8c03bfcfd6bc00b00041e1a2313ef
Riley <92556890+user4708@users.noreply.github.com>
2023-02-26 17:35:14 -0600

    Update CalcTest.java

--- CalcTest.java
+++ CalcTest.java
@@ -4,33 +4,103 @@
 
 package junitTests;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.Assert.*;
-import static org.junit.Assert.fail;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-class CalcTest {
+/**
+ *
+ * @author Riley
+ */
+public class CalcTest {
+    
+    public CalcTest() {
+    }
 
-	   @Test public void testAdd(){
-	      assertTrue ("Calc sum incorrect", 4 == Calc.add (2, 2));
-	   }
-	   
-	   @Test public void testSubstract(){
-	      assertTrue ("Calc substraction incorrect", 0 == Calc.substract (2, 2));
-	   }
-	   
-	   @Test public void testMultiply(){
-	      assertTrue ("Calc multiplication incorrect", 12 == Calc.multiply (2, 6));
-	   }
-	   
-	   @Test public void testDivide(){
-		  try {
-			  assertTrue ("Calc division incorrect", 10 == Calc.divide (100, 10));  //Causes underflow
-		  }
-		  catch(ArithmeticException e) {
-			  return;
-		  }
-		  fail("ArithmeticException expected");
-	   }
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
 
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    
+//    @BeforeAll
+//    public static void setUpClass() {
+//    }
+//    
+//    @AfterAll
+//    public static void tearDownClass() {
+//    }
+//    
+//    @BeforeEach
+//    public void setUp() {
+//    }
+//    
+//    @AfterEach
+//    public void tearDown() {
+//    }
+
+    /**
+     * Test of add method, of class Calc.
+     */
+    @org.junit.jupiter.api.Test
+    public void testAdd() {
+        System.out.println("add");
+        int a = 2;
+        int b = 2;
+        int expResult = 4;
+        int result = Calc.add(a, b);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of substract method, of class Calc.
+     */
+    @org.junit.jupiter.api.Test
+    public void testSubstract() {
+        System.out.println("substract");
+        int a = 6;
+        int b = 3;
+        int expResult = 3;
+        int result = Calc.substract(a, b);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of multiply method, of class Calc.
+     */
+    @org.junit.jupiter.api.Test
+    public void testMultiply() {
+        System.out.println("multiply");
+        int a = 6;
+        int b = 2;
+        int expResult = 12;
+        int result = Calc.multiply(a, b);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of divide method, of class Calc.
+     */
+    @org.junit.jupiter.api.Test
+    public void testDivide() {
+        System.out.println("divide");
+        int a = 100;
+        int b = 10;
+        int expResult = 10;
+        int result = Calc.divide(a, b);
+        assertEquals(expResult, result);
+    }
 }
