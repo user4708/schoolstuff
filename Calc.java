ee07a4866782a2b266b239d76b7e8aafffc11753
Riley <92556890+user4708@users.noreply.github.com>
2023-02-26 17:25:34 -0600

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -4,24 +4,22 @@
 
 package junitTests;
 
-public class Calc {   //Fixed calc1 for it to pass the tests.
+public class Calc{
 
-	static public int add (int a, int b)
-	{
+	static public int add (int a, int b){
 		return a + b;
 	}	
 	
-	static public int substract(int a, int b)
-	{
+	static public int substract(int a, int b){
 		return a - b;
 	}
 	
-	static public int multiply(int a, int b) {
+	static public int multiply(int a, int b){
 		return a * b;
 	}
 	
-	static public int divide(int a, int b) {
-		if(b == 0) throw new ArithmeticException("Calc.divide");
+	static public int divide(int a, int b){
+		if(b == 0) throw new ArithmeticException("Calc.divide"); // can't divide by zero
 		int c = a / b;
 		if(a != 0 && c == 0) throw new ArithmeticException("Calc.divide");
 
